7d978cd5c67cc90a5ab900b30b613b2108143356
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-04-01 14:41:15 -0700

    participants displaying on workshop page, and deleting on click

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -1,4 +1,4 @@
-import { checkAuth, logout } from "../fetch-utils.js";
+import { checkAuth, deleteParticipant, getWorkshops, logout } from '../fetch-utils.js';
 
 
 checkAuth();
@@ -10,3 +10,43 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
+
+async function fetchAndDisplayWorkshops() {
+    const workshops = await getWorkshops();
+
+    workshopListEl.textContent = '';
+
+    for (let workshop of workshops) {
+        const workshopEl = document.createElement('div');
+        const nameEl = document.createElement('h3');
+        const participantsEl = document.createElement('div');
+
+        participantsEl.classList.add('participants');
+        workshopEl.classList.add('workshop');
+
+        nameEl.textContent = workshop.name;
+
+
+        for (let participant of workshop.participants) {
+            const participantEl = document.createElement('div');
+
+            participantEl.classList.add('participant');
+            participantEl.textContent = participant.name;
+
+            participantEl.addEventListener('click', async () => {
+                await deleteParticipant(participant.id);
+                await fetchAndDisplayWorkshops();
+
+            });
+            participantsEl.append(participantEl);
+        }
+        workshopEl.append(nameEl, participantsEl);
+        workshopListEl.append(workshopEl);
+    }
+}
+
+window.addEventListener('load', async e => {
+    e.preventDefault();
+    fetchAndDisplayWorkshops();
+    
+});
